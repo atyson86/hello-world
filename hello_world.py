2ede87757e8010dd18f7a5e95373ed6e3d5da431
Gannon Leech <gleech@bowdoin.edu>
2021-02-18 15:47:56 -0500

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -4,7 +4,7 @@ Team members: Alex Tyson, Gannon Leech, Josh Lin
 
 
 def main():
-    print("Hello, this is for PQ0!")
+    print("Hello, this is for Party Quest 0!")
 
 if __name__ == "__main__":
     main()
