20eda82d694921e9f0bf7c26d7b2fbaea136f3d0
Alex Tyson <atyson@bowdoin.edu>
2021-02-18 14:34:10 -0500

    Update hello_world.py
    Changed the greeting a second time.

--- hello_world.py
+++ hello_world.py
@@ -4,7 +4,7 @@ Team members: Alex Tyson, Gannon Leech, Josh Lin
 
 
 def main():
-    print("Hello, this is for PQ0!")
+    print("Hello, Josh! Thank's for making the first change.")
 
 if __name__ == "__main__":
     main()
